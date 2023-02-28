383ca014430c946df37d08bc7ccd942fa49f9722
Vasil Ivanov <59696861+vasilvas99@users.noreply.github.com>
2023-02-28 11:58:40 +0200

    Update setup_container_can.sh

--- setup_container_can.sh
+++ setup_container_can.sh
@@ -8,15 +8,15 @@ set -e
 CONTNAME=$1
 CONTPID=$(kanto-cm -n $CONTNAME get | jq " .state.pid") 
 
-# setup to virtual linked cans: vxcan0 and vxcan1
+# setup to virtual linked cans vxcan0 and vxcan1
 ip link add vxcan0 type vxcan peer name vxcan1
-ip link set vxcan0 up
 
 # send vxcan1 to the container namespace
 ip link set vxcan1 netns $CONTPID
+ip link set vxcan0 up
 nsenter -t $CONTPID -n ip link set vxcan1 up
 
-# setup routing from and to the physical can0 and vxcan
+# setup physical-to-virtual-can routing
 modprobe can-gw
-cangw -A -s can0 -d vxcan0 -e
-cangw -A -s vxcan0 -d can0 -e
+sudo cangw -A -s can0 -d vxcan0 -e
+sudo cangw -A -s vxcan0 -d can0 -e
